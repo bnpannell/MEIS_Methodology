72874c92ec4d49120be8dea38410f7da844545ae
sumeetkbedi <sumeetkb24@gmail.com>
2021-11-19 10:39:38 -0800

    Updated code so that it now works!

--- test.R
+++ test.R
@@ -2,20 +2,27 @@ library(httr)
 library(jsonlite)
 library(tidyverse)
 
-body <- list(filters=list(prime_award_types=c("A", "B", "C", "D"),
-                       data_type="action_date",
-                       date_range=list(start_date="2019-10-01",
-                                       end_date="2020-09-30"),
-                       recipient_locations = list(country ="United States", 
-                                                  state = "California",
-                                                  county = "Del Norte"),
-                       agencies = list(type="awarding",
-                                          tier="toptier",
-                                          name="Department of Homeland Security",
-                                          toptier_name="Department of Homeland Security")
-),
-file_format="csv"
+body = list(
+  filters = list(
+    prime_award_types = c("A", "B", "C", "D"),
+    agencies = data.frame(
+      type = "awarding",
+      tier = "toptier",
+      name = "Department of Homeland Security",
+      toptier_name = "Department of Homeland Security"
+    ),
+    date_type = "action_date",
+    date_range = list(start_date = "2019-10-01",
+                      end_date = "2020-09-30"),
+    recipient_locations = data.frame(country = "United States",
+                                     state = "California",
+                                     county = "Del Norte")
+  ),
+  file_format = "csv"
 )
-
-r2 <- POST("https://api.usaspending.gov/api/v2/bulk_download/awards/", body = body)
-content(r2)$results
+toJSON(body, pretty=T)
+r2 <-
+  POST("https://api.usaspending.gov/api/v2/bulk_download/awards/",
+       body = body,
+       encode="json",
+       verbose())
