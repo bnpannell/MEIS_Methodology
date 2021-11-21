0215be46e012988d972fe5c732da6744e87e0e44
sumeetkbedi <sumeetkb24@gmail.com>
2021-11-20 18:19:23 -0800

    Updated with code to download usaspending data for DHS contracts in Del Norte county, CA.

--- test.R
+++ test.R
@@ -14,15 +14,22 @@ body = list(
     date_type = "action_date",
     date_range = list(start_date = "2019-10-01",
                       end_date = "2020-09-30"),
-    recipient_locations = data.frame(country = "United States",
-                                     state = "California",
-                                     county = "Del Norte")
+    recipient_locations = data.frame(country = "USA",
+                                     state = "CA",
+                                     county = "015")
   ),
   file_format = "csv"
 )
-toJSON(body, pretty=T)
+
 r2 <-
   POST("https://api.usaspending.gov/api/v2/bulk_download/awards/",
        body = body,
        encode="json",
        verbose())
+
+content(r2)
+
+status_check <- GET(url = content(r2)$status_url)
+content(status_check)$status
+
+content(r2)$file_url
