6029cc77008c55e300f2919f07c00e0960ee74e7
bnpannell <britnee.pannell@library.opr.gov>
2021-12-07 11:13:13 -0800

    edits

--- src/filter_by_ppp_location.R
+++ src/filter_by_ppp_location.R
@@ -4,6 +4,13 @@
 #Filter both files by Recipient and Planed Performance location, both in the state of CA- check location variables and document
 #Output filtered data with new name into temp folder- follow some output format. Define name in parameters file
 
+
+# DOD GRANTS
+DODGrants <- DODGrants21 %>%
+  filter(primary_place_of_performance_state_name == "CALIFORNIA") %>%
+  select("federal_action_obligation", "recipient_name", "recipient_county_name", "recipient_congressional_district", 
+         "business_types_description") %>% #select needed columns
+  rename(county = "recipient_county_name", district = "recipient_congressional_district")
 # DOD Contracts
 DODContracts <- DODContracts21 %>% 
   filter(primary_place_of_performance_state_name == "CALIFORNIA") %>%
