cb65caac9c2cb840ac703107dcd2978672e09fa3
sumeetkbedi <sumeetkb24@gmail.com>
2021-12-28 10:03:50 -0800

    added equal sign

--- src/filter_usaspending.R
+++ src/filter_usaspending.R
@@ -3,7 +3,7 @@
 filter_usaspending <- function(file_name, state, doe_filters, filters, out_name) {
   read.csv(file.path(getwd(), "data", "temp", file_name))  %>%
     filter(primary_place_of_performance_state_name == state) %>%
-    filter(!(awarding_agency_name = doe & !(funding_office_name %in% doe_filters))) %>%
+    filter(!(awarding_agency_name == doe & !(funding_office_name %in% doe_filters))) %>%
     select(all_of(filters)) %>%
     write.csv((file.path(getwd(), "data", "temp", out_name)))
   }
