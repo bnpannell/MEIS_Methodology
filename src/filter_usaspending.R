382bd86f18e6e7d53c3ed44bbd22b0c556472297
sumeetkbedi <sumeetkb24@gmail.com>
2021-12-28 09:56:23 -0800

    modified code

--- src/filter_usaspending.R
+++ src/filter_usaspending.R
@@ -1,8 +1,9 @@
-#Defines function "filter_ppp" 
+#Defines function "filter_usaspending" 
 
-filter_ppp <- function(file_name, state, filters, out_name) {
+filter_usaspending <- function(file_name, state, doe_filters, filters, out_name) {
   read.csv(file.path(getwd(), "data", "temp", file_name))  %>%
     filter(primary_place_of_performance_state_name == state) %>%
+    filter(!(awarding_agency_name = doe & !(funding_office_name %in% doe_filters))) %>%
     select(all_of(filters)) %>%
     write.csv((file.path(getwd(), "data", "temp", out_name)))
   }
