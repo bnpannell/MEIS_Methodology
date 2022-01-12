78a7a6fcfa43be88bd9da9e10b786afedb8cde1f
sumeetkbedi <sumeetkb24@gmail.com>
2022-01-12 12:19:43 -0800

    modified code to get concatenated usaspending data written into a CSV

--- DEPRECIATED_run_analysis_master.R
+++ DEPRECIATED_run_analysis_master.R
@@ -13,11 +13,11 @@ library(dplyr)
 library(openxlsx) 
 
 
-
 ##Load Parameters##
 source("src/parameters.R")
 
-##Load Scripts##
+
+##Load Scripts to retrieve usaspending from API and filter the data down##
 #source("src/obtain_usaspending.R")
 source("src/filter_usaspending.R")
 
@@ -30,16 +30,18 @@ gfile_name <- list.files(path = file.path(getwd(), "data", "temp"), pattern = pa
 filter_usaspending(cfile_name, state, doe_offices, contract_columns, paste0("DEPRECIATED_", c_out_name))
 filter_usaspending(gfile_name, state, doe_offices, grant_columns, paste0("DEPRECIATED_", g_out_name))
 
+
 ## Run error check on data - including manual fixes mentioned in methodology
 source("src/depreciated/DEPRECIATED_error_check_contracts.R")
 source("src/depreciated/DEPRECIATED_error_check_grants.R")
 
-## Run concatenate function to combine usaspending contracts and grants data
-source("src/concatenate_usaspending.R")
-c_usaspending(pattern = paste0(year, "_DEPRECIATED.+\\.csv"))
 
-#ufile_name <- list.files(path = file.path(getwd(), "data", "temp"), pattern = paste0(year, "_DEPRECIATED.+\\.csv"), full.names = TRUE)
-#c_usaspending(ufile_name, u_out_name)
+## Run concatenate function to combine usaspending contracts and grants data, and write into CSV
+source("src/concatenate_usaspending.R")
+concatenated_usaspending <- c_usaspending(pattern = paste0(year, "_DEPRECIATED.+\\.csv"))
+
+write.csv(concatenated_usaspending, file.path(getwd(), "data", "temp", paste0("DEPRECATED_", u_out_name))) 
+
 
 
 
