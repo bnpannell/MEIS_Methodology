c7b734494d496e87112a87487c3332b777b2d0fa
sumeetkbedi <sumeetkb24@gmail.com>
2022-01-18 15:03:02 -0800

    modified split usaspending code

--- src/split_usaspending.R
+++ src/split_usaspending.R
@@ -1,15 +1,11 @@
 #Defines functions "split_usaspending" and "split_doespending"
 
 split_usaspending <- function(file_name, is_doe) {
-  read.csv(file.path(getwd(), "data", "temp", file_name)) %>%
-    if(is_doe == "yes") {
-    filter((awarding_agency_name == doe))
-    }
+  temp_data = read.csv(file.path(getwd(), "data", "temp", file_name))
+  {if(is_doe) {
+    filter(temp_data, awarding_agency_name == doe)
+  }
     else {
-    filter((awarding_agency_name != doe))
-    }
+      filter(temp_data, awarding_agency_name != doe)
+    }}
 }
-
-
-
-
