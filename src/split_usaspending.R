52a1521014204105d1d3ef6ac1caa0b662e4076d
bnpannell <britnee.pannell@library.opr.gov>
2022-01-18 12:27:00 -0800

    local edits

--- src/split_usaspending.R
+++ src/split_usaspending.R
@@ -1,11 +1,15 @@
 #Defines functions "split_usaspending" and "split_doespending"
 
-split_usaspending <- function(file_name) {
+split_usaspending <- function(file_name, is_doe) {
   read.csv(file.path(getwd(), "data", "temp", file_name)) %>%
-    filter(!(awarding_agency_name == doe))
+    if(is_doe == "yes") {
+    filter((awarding_agency_name == doe))
+    }
+    else {
+    filter((awarding_agency_name != doe))
+    }
 }
 
-split_doespending <- function(file_name) {
-  read.csv(file.path(getwd(), "data", "temp", file_name)) %>%
-    filter(awarding_agency_name == doe)
-}
+
+
+
