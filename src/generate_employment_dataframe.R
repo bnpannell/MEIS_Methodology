bf6b88b27c4b0e014681e996f633002208282ece
sumeetkbedi <sumeetkb24@gmail.com>
2022-01-26 16:30:46 -0800

    modified employment code to make it work. and created template emp CSV file

--- src/generate_employment_dataframe.R
+++ src/generate_employment_dataframe.R
@@ -13,48 +13,36 @@ civilian_emp = dod_emp + dhs_emp + va_emp
 doe_emp = state_emp[1,9] * doe_ns_adjustment
 
 ##Begin apportioning military and civilian employment by county and district, sectioning off based on employment type##
-##Start out with military employment - use ACS data and apportion by employed armed forces. 
-#First clean up the ACS dataframe to format entries as needed.
+##Start out with military employment - use ACS data. First clean up the ACS dataframe to format entries as needed.
 
-read_excel(file.path(getwd(), "data", "temp", acs)) %>%
+acs_data <- read_excel(file.path(getwd(), "data", "temp", acs)) %>%
   select(geography, armed_forces_employed)
 
-dod_shares_county <- read_excel(file.path(getwd(), "data", "raw", dod_shares), sheet = 1)
-dod_shares_district <- read_excel(file.path(getwd(), "data", "raw", dod_shares ), sheet = 2)
-
-fedprop <- read.csv(file.path(getwd(), "data", "temp", fed_prop), stringsAsFactors = FALSE) %>%  
-  select(Reporting.Agency, Reporting.Bureau, Real.Property.Type, Real.Property.Use, Square.Feet..Buildings.,  
-         Street.Address, State.Name, County.Name, Zip.Code, Congressional.District) %>%
-  rename(Department = Reporting.Agency, Prop_type=Real.Property.Type,
-         Prop_use=Real.Property.Use, Sq_ft=Square.Feet..Buildings., Add=Street.Address, county=County.Name,
-         district=Congressional.District) %>% 
-  filter(State.Name == state) %>% #select only entries in California 
-  filter(Prop_type == "Building") %>% #select only buildings 
-  filter(Department == "VETERANS AFFAIRS" | Department == "HOMELAND SECURITY")
-
-acs$geography <- gsub(pattern = " County, California", replacement = "", acs$geography)
-acs$geography <- gsub(pattern = "Congressional District ([0-9]+) \\(116th Congress\\), California",
-                           -                          replacement = "\\1",
-                           -                          acs$geography)
-acs$geography = toupper(acs$geography)
+acs_data$geography <- gsub(pattern = " County, California", replacement = "", acs_data$geography)
+acs_data$geography <- gsub(pattern = "Congressional District ([0-9]+) \\(116th Congress\\), California",
+                           replacement = "\\1", acs_data$geography)
+acs_data$geography = toupper(acs_data$geography)
 
 #Calculate armed_forces_percent by dividing each geography's armed_forces_employed by the statewide total. Then multiply that percent by mili_emp to get military employees for each local geography.
-acs <- acs %>% mutate(armed_forces_percent = (acs$armed_forces_employed / acs$armed_forces_employed[112]),
+acs_data <- acs_data %>% mutate(armed_forces_percent = (acs_data$armed_forces_employed / acs_data$armed_forces_employed[112]),
                       military_personnel = mili_emp * armed_forces_percent) %>%
   select(geography, military_personnel)
 
 #Separate county military personnel and district military personnel into 2 dataframes. Localized military personnel is done.
-mili_county <- acs %>% filter(is.na(as.numeric(acs$geography))) %>%
+mili_county <- acs_data %>% filter(is.na(as.numeric(acs_data$geography))) %>%
   filter(!(geography == "CALIFORNIA"))
 mili_county <- mili_county %>%
   rename(county = geography, mili_emp = military_personnel)
 
-mili_district <- acs %>% filter(!is.na(as.numeric(acs$geography)))
+mili_district <- acs_data %>% filter(!is.na(as.numeric(acs_data$geography)))
 mili_district <- mili_district %>%
   rename(district = geography, mili_emp = military_personnel)
 
 
 ##Next is DOD civilian employment - use DOD County Shares Excel file##
+dod_shares_county <- read_excel(file.path(getwd(), "data", "raw", dod_shares), sheet = 1)
+dod_shares_district <- read_excel(file.path(getwd(), "data", "raw", dod_shares ), sheet = 2)
+
 #Start out by calculating DOD employment by county - multiply the county share percentage by the statewide DOD employees to get each county's DOD employees
 dod_county <- dod_shares_county %>%
   mutate("dod_emp" = dod_shares_county$Share * dod_emp) %>%
@@ -74,6 +62,15 @@ dod_county <- dod_county %>%
   rename(county = "geography")
 
 ##Last employment data to localize is VA and DHS employment - use fedprop CSV file##
+fedprop <- read.csv(file.path(getwd(), "data", "temp", fed_prop), stringsAsFactors = FALSE) %>%  
+  select(Reporting.Agency, Reporting.Bureau, Real.Property.Type, Real.Property.Use, Square.Feet..Buildings.,  
+         Street.Address, State.Name, County.Name, Zip.Code, Congressional.District) %>%
+  rename(Department = Reporting.Agency, Prop_type=Real.Property.Type,
+         Prop_use=Real.Property.Use, Sq_ft=Square.Feet..Buildings., Add=Street.Address, county=County.Name,
+         district=Congressional.District) %>% 
+  filter(State.Name == state) %>% #select only entries in California 
+  filter(Prop_type == "Building") %>% #select only buildings 
+  filter(Department == "VETERANS AFFAIRS" | Department == "HOMELAND SECURITY")
 ##First start with working through VA - limit fedprop to VA entries, and clean up some missing 0's in districts column.
 va_fedprop <- fedprop %>%
   filter(Department == "VETERANS AFFAIRS")
