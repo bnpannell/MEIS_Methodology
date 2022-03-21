fd33c6e34584efe5966cef3c827b4d6a08136c21
Bedi <sumeet.bedi@library.ca.gov>
2022-03-21 16:36:40 -0700

    modified code to work through censusapi package

--- src/census_api_call.R
+++ src/census_api_call.R
@@ -1,42 +1,24 @@
-##Code that calls to Census API for data on veterans in California##
-
-library(jsonlite)
-library(httr)
-
-
-
-##Method #1: Lines 8-16 work to read the Census API into a dataframe... only need the jsonlite package for this
-
-data <- url("https://api.census.gov/data/2020/acs/acs5?get=B21001_002E,NAME&for=county:*&in=state:06")
-
-datanew <- fromJSON(data)
-
-dataframe1 <- as.data.frame(datanew)
-names(dataframe1) <- c("total_vets", "county", "state_fips", "county_fips")  
-dataframe1 <- dataframe1[-c(1),]
-
-
-
-##Method #2: Lines 20-31 also work to read the Census API into a dataframe, but using both httr and jsonlite
-
-censusapi_url <- paste0("https://api.census.gov/data/2020/acs/acs5?get=B21001_002E,NAME&for=county:*&in=state:06")
-result <- httr::GET(censusapi_url)
-
-#Check out result - in this list, we need the "content" in it. That holds the data.
-content <- httr::content(result, as ="text")
-content_json <- jsonlite::fromJSON(content)
-
-dataframe2 <- as.data.frame(content_json)
-names(dataframe2) <- c("total_vets", "county", "state_fips", "county_fips")  
-dataframe2 <- dataframe2[-c(1),]
-
-
-
 ##If we went the route of the census API R package...
 
-#First, install and load the census API package
-
-#install.packages("censusapi")
-#library("censusapi")
+# Add key to .Renviron
+Sys.setenv(CENSUS_KEY= "8df2ad0b1d54d9a4a19c1d97bed2e94e44995571")
+# Reload .Renviron
+readRenviron("~/.Renviron")
+# Check to see that the expected key is output in your R console
+Sys.getenv("CENSUS_KEY")
 
 #Look at the list of endpoints available
+view(listCensusApis())
+
+# API Call
+vets <- getCensus(
+  name = "acs/acs5",
+  vintage = 2020, 
+  vars = c("NAME", "B21001_002E"), 
+  region = "county (or part):*", 
+  regionin = "state:06+congressional district:51")
+
+listCensusMetadata(
+  name="acs/acs5",
+  vintage = 2020,
+  type = "geography")
