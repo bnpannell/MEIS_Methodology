4cabb3c6756244329da88e20ed961c13803ff566
bnpannell <britnee.pannell@library.opr.gov>
2022-03-22 18:15:44 -0700

    edits

--- src/make_va_benefits_cw.R
+++ src/make_va_benefits_cw.R
@@ -1,4 +1,10 @@
 
 
 districts_vets$county_or_part <- as.numeric(districts_vets$county_or_part)
+dist_vets <- rename(districts_vets, dist_vets = B21001_002E)
 counties_vets$county <- as.numeric(counties_vets$county)
+cnty_vets <- rename(counties_vets, cnty_vets = B21001_002E)
+
+result <- merge(x=dist_vets, y=cnty_vets, by.x="county_or_part", by.y="county", x.all=FALSE, y.all=FALSE)
+
+#work on making new column and calculating % of county total next 
