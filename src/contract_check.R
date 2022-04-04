6c5f0cd171153bb41fd259b12bcc1ec8dc4c3332
bnpannell <britnee.pannell@library.opr.gov>
2022-04-04 09:33:31 -0700

    add content

---
+++ src/contract_check.R
@@ -0,0 +1,21 @@
+#Scratch file for parsing thru construction NAICs codes and parsing them out by keywords
+
+#Load in contracts_errors dataframe
+#Load in list- from parameters file?
+#Use grepl(pattern, string, ignore.case=FALSE) to match patterns- returns true if pattern is found in the string 
+
+#Loop thru line by line, if true put record in dataframe A, if false record to dataframe B? 
+
+myfilter <- function(patterns, data){
+  val <- rep(FALSE, length(data))
+  if(is.character(patterns)){
+    for(i in 1:length(patterns)){
+      val <- val | grepl(patterns[i], data)
+    }
+  } else {
+    stop('pattern must be a character vector')
+  }
+  return(val)
+}
+patterns = c("escholarship", "trid", "doi")
+test <- urls[myfilter(patterns = patterns, data = urls$publink),]
