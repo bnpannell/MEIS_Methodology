5fe81449159ed3744052d2e60e0bae47133150e6
sumeetkbedi <sumeet.bedi@library.ca.gov>
2022-05-19 18:13:01 -0700

    removed file related to OPM FOIA code and small edits to census api call code

--- src/census_api_call.R
+++ src/census_api_call.R
@@ -1,5 +1,5 @@
 #Add key to .Renviron
-Sys.setenv(CENSUS_KEY= c_key)
+Sys.setenv(CENSUS_KEY = c_key)
 
 #Reload .Renviron
 readRenviron("~/.Renviron")
@@ -12,13 +12,6 @@ regionin_call = paste0(state_call, "+congressional district:", dist_list)
 YEAR = as.integer(f_year)
 
 #API Call - this is for apportioning VA direct benefits
-districts_vets <- getCensus(
-  name = "acs/acs5",
-  vintage = YEAR, 
-  vars = c("NAME", "B21001_002E"), 
-  region = "county (or part):*", 
-  regionin = regionin_call)
-
 counties_vets <- getCensus(
   name = "acs/acs5",
   vintage = YEAR, 
@@ -26,7 +19,14 @@ counties_vets <- getCensus(
   region = "county:*", 
   regionin = state_call)
 
-#API Call - this is for apportioning VA and DHS employment
+districts_vets <- getCensus(
+  name = "acs/acs5",
+  vintage = YEAR, 
+  vars = c("NAME", "B21001_002E"), 
+  region = "county (or part):*", 
+  regionin = regionin_call)
+
+#API Call - this is for apportioning military employment
 counties_armedforces <- getCensus(
   name = "acs/acs5",
   vintage = YEAR, 
