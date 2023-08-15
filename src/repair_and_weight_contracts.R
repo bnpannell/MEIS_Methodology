02e3367d429adc46f95cd5986ffa74945c54f07f
sumeetkbedi <sumeet.bedi@library.ca.gov>
2023-08-15 15:55:42 -0700

    Revised repair and weights contracts code to properly identify the "Ratio" column for calculations

--- src/repair_and_weight_contracts.R
+++ src/repair_and_weight_contracts.R
@@ -3,11 +3,11 @@
 clean_contracts <- read.csv(file.path(temp_path, paste0(f_year, clean_c_data)))
 
 #Modify CewAvgRatio column so any NA values are simply 1
-clean_contracts$CewAvgRatio[is.na(clean_contracts$CewAvgRatio)] <- 1
+clean_contracts$Ratio[is.na(clean_contracts$Ratio)] <- 1
 
 #Multiply the spending column by the CewAvgRatio column to get the weighted spending data
 clean_contracts <- clean_contracts %>%
-  mutate(fao_weighted = federal_action_obligation * CewAvgRatio)
+  mutate(fao_weighted = federal_action_obligation * Ratio)
 
 #Drop unneeded columns from dataframe and write to file
 clean_contracts <- clean_contracts %>%
